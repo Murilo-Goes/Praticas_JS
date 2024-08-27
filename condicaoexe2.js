6932edc2feacaca297a4ff294cd98181ee4d6ce1
Murilo Góes <murilogoes25@gmail.com>
2024-08-27 20:17:35 -0300

    up

---
+++ condicaoexe2.js
@@ -0,0 +1,9 @@
+var time = 'ganhar'
+
+if (time == 'perder') {
+    console.log('O time é mais fraco de caldo de bila');
+} if (time == 'ganhar') {
+    console.log('O Serjão vai vestir a camisa do Lion');
+} else if (time == 'empatar') {
+    console.log('Robin vai virar o cara de peixe');
+} 
