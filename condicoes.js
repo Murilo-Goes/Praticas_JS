d962ceb4117fc9bb800d398700915ffe238b3855
Murilo Góes <murilogoes25@gmail.com>
2024-08-27 19:45:34 -0300

    condicoes

---
+++ condicoes.js
@@ -0,0 +1,10 @@
+// console.log('estou tremendo na base com JS ')    // comando para escrever na tela 
+
+var velo = 10
+var tempo = 10
+
+var distancia = velo * tempo
+if (distancia >= 100) {   // condicao simples pois so teve uma condicao 
+
+    console.log('A distância percorrida é:', distancia, 'km')
+}
